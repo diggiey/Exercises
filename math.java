f9f8038ee232f7d3edc5e1a8afee6b08b6257094
Andrew <diggeiy77@gmail.com>
2013-09-09 20:07:56 -0600

    Finished both exercises

--- math.java
+++ math.java
@@ -4,14 +4,19 @@ public class math {
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
 
-		System.out.println("Enter x:");
-		int x = reader.nextInt();
-		
-		while (x > Math.pow(10, -12)) {
-			double n = 1;
-			for ( int i=1; i<exponet; i++) {
-				
-			}
+		System.out.print("Enter x:");
+		double x = reader.nextDouble();
+		double n = 1.0;
+		double sum = 1.0;
+		double factorial = n;
+		double term = Math.pow(x, n) / n;
+
+		while (term > Math.pow(10.0, -12.0)) {
+			term = Math.pow(x, n) / factorial;
+			sum = sum + term;
+			System.out.println(sum);
+			n++;
+			factorial *= n;
 		}
 	}
 }
